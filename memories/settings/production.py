fa6e94239f60f4581e05472fd832263f4530065c
danangharissetiawan <mr.danangharissetiawan@gmail.com>
2021-10-12 21:14:58 +0700

    change production

--- memories/settings/production.py
+++ memories/settings/production.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'memoriesrisset.herokuapp.com', 'memories.risset.me' ]
 
-# PRODUCTION_APPS = ['whitenoise.runserver_nostatic']
+PRODUCTION_APPS = ['whitenoise.runserver_nostatic']
 
 DEVELOPMENT_MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
