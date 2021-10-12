4158cb3bb0dc2e42ceed6eeed3f17f169be16633
danangharissetiawan <mr.danangharissetiawan@gmail.com>
2021-10-12 21:29:12 +0700

    test

--- memories/settings/production.py
+++ memories/settings/production.py
@@ -6,13 +6,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'memoriesrisset.herokuapp.com', 'memories.risset.me' ]
 
-PRODUCTION_APPS = ['whitenoise.runserver_nostatic']
+# PRODUCTION_APPS = ['whitenoise.runserver_nostatic']
 
-DEVELOPMENT_MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
+# DEVELOPMENT_MIDDLEWARE = [
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+# ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -20,7 +20,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DATABASES = {
     'default': {
@@ -28,11 +28,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 COMPRESS_ENABLED = False
 COMPRESS_OFFLINE = False
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
