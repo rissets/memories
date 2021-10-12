803ca759e56511f340b3e2dd47b1965f7cf52a34
danangharissetiawan <mr.danangharissetiawan@gmail.com>
2021-10-12 15:51:58 +0700

    STATICFILES_STORAGE

--- memories/settings/base.py
+++ memories/settings/base.py
@@ -166,7 +166,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
