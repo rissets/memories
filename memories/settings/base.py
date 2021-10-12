5a30fb3f1010b43d79111c39431130ee2a57a63a
danangharissetiawan <mr.danangharissetiawan@gmail.com>
2021-10-12 15:56:48 +0700

    STATICFILES_DIRS

--- memories/settings/base.py
+++ memories/settings/base.py
@@ -171,8 +171,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/"static", ]
+# STATICFILES_DIRS = [BASE_DIR/"static", ]
 STATIC_ROOT = (BASE_DIR/'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'memories/static')]
+
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
